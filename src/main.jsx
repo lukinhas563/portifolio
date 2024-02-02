8b7b969ee044df2e1bd0f2910236d3e925b324e7
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2024-02-02 11:23:07 -0300

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import { AuthGoogleProvider } from './context/authGoogle';
 import 'react-toastify/dist/ReactToastify.css'
-import './index.css'
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
